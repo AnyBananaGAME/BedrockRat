36ccbed5272dba64c52058a994d5d67f50e01cb5
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-02-17 00:11:55 +0200

    update messy code

--- libs/MiningLib.js
+++ libs/MiningLib.js
@@ -1,37 +1,34 @@
-const { ClientAuthInput } = require("../ClientAuthInput");
-const sleep = ms => new Promise((resolve) => setTimeout(resolve, ms))
-const util = require('util')
+const { ClientAuthInput } = require('../network/packets/ClientAuthInput')
 
 module.exports = (client) => {
-    client.mine = async (position) => {
-        let tick = client.data.tick;
-        let client_position = client.data.position;
+  client.mine = async (position) => {
+    let tick = client.data.tick
 
-        let x = position.x;
-        let y = position.y;
-        let z = position.z;
+    const x = position.x
+    const y = position.y
+    const z = position.z
 
-        const stop = tick + 5;
+    const stop = tick + 5
 
-        let actions1 = [ { action: 'start_break', position: { x: x, y: (y), z: z }, face: 3 }, { action: 'crack_break', position: { x: x, y: (y), z: z }, face: 3 } ]
-        
-        let packet = new ClientAuthInput(client, actions1, position);
-        client.queue("player_auth_input", packet.create());
+    const actions1 = [{ action: 'start_break', position: { x, y, z }, face: 3 }, { action: 'crack_break', position: { x, y, z }, face: 3 }]
 
-        const cracks = setInterval(() => {
-            if (++tick < stop){
-                let actions2 = [{ action: 'crack_break', position: { x: x, y: (y) , z: z }, face: 3}]
-                
-                let packet2 = new ClientAuthInput(client, actions2, position);
-                client.queue("player_auth_input", packet2.create());
-            }  else {
-                clearInterval(cracks);
-                let actions3 = [ { action: 'stop_break' }, { action: 'crack_break', position: { x: x, y: (y), z: z }, face: 3},]
+    const packet = new ClientAuthInput(client, actions1, position)
+    client.queue('player_auth_input', packet.create())
 
-                let packet3 = new ClientAuthInput(client, actions3, position, true);
-                client.queue("player_auth_input", packet3.create());
-            }
-        }, 50)
-        console.log("Finished queueing packets")
-    }
+    const cracks = setInterval(() => {
+      if (++tick < stop) {
+        const actions2 = [{ action: 'crack_break', position: { x, y, z }, face: 3 }]
+
+        const packet2 = new ClientAuthInput(client, actions2, position)
+        client.queue('player_auth_input', packet2.create())
+      } else {
+        clearInterval(cracks)
+        const actions3 = [{ action: 'stop_break' }, { action: 'crack_break', position: { x, y, z }, face: 3 }]
+
+        const packet3 = new ClientAuthInput(client, actions3, position, true)
+        client.queue('player_auth_input', packet3.create())
+      }
+    }, 50)
+    console.log('Finished queueing packets')
+  }
 }
