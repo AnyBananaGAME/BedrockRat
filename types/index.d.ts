54de2e952219056ef01147b1aeb62dd69650c986
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-02-17 14:48:02 +0200

    Types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,25 @@
 import { Client } from 'bedrock-protocol';
 
+
+/**
+ * Returns the Position of something.
+ */
+interface Position {
+    x: number;
+    y: number;
+    z: number;
+}
+
 export interface BedrockRat extends Client {
-    mine(blockPosition: string): void;
+    data: {
+        position: Position;
+        tick: number;
+        debug: boolean;
+        runtime_entity_id: number;
+        yaw: number;
+        pitch: number;
+    }
+    mine(blockPosition: string): Promise<void>;
 }
 
 export default BedrockRat;
