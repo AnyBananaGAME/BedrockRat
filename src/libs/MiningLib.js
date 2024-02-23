880a7bdc11f5638873fc1224f9a9adf7de56d8ad
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-02-23 02:13:16 +0200

    Some updates to Mining lib

--- src/libs/MiningLib.js
+++ src/libs/MiningLib.js
@@ -1,6 +1,4 @@
-const { ClientAuthInput } = require('../network/packets/ClientAuthInput')
 const sleep = ms => new Promise((resolve) => setTimeout(resolve, ms))
-
 /** @param {import("../../types/index").BedrockRat} client */
 module.exports = (client) => {
   client.mine = async (position) => {
@@ -13,31 +11,56 @@ module.exports = (client) => {
 
       const stop = tick + 5
 
-      const actions1 = [{ action: 'start_break', position: { x, y, z }, face: 3 }, { action: 'crack_break', position: { x, y, z }, face: 2 }]
+      const temp = calculatePitchAndYaw(client.playerState.position, position)
+      client.playerState.pitch = temp.pitch
+      client.playerState.yaw = temp.yaw
 
       if (position.x > client.data.position.x + 5 || position.z > client.data.position.z + 5) {
         reject(new Error('The block is too far from the player.\nMax distance is 5 blocks.'))
       }
 
-      const data = calculatePitchAndYaw(client.data.position, position)
-      client.data.yaw = data.yaw
-      client.data.pitch = data.pitch
+      const face = 0
 
-      const packet = new ClientAuthInput(client, actions1, position)
-      client.queue('player_auth_input', packet.create())
+      client.once('pre_player_auth_input', (eventTwo) => {
+        const data = eventTwo.data
+        data.block_action = [{ action: 'start_break', position: { x, y, z }, face }, { action: 'crack_break', position: { x, y, z }, face: 2 }]
+        data.input_data.block_action = true
+      })
 
       const cracks = setInterval(async () => {
         if (++tick < stop) {
-          const actions2 = [{ action: 'crack_break', position: { x, y, z }, face: 2 }]
+          client.once('pre_player_auth_input', (cancelable) => {
+            const data = cancelable.data
 
-          const packet2 = new ClientAuthInput(client, actions2, position)
-          client.queue('player_auth_input', packet2.create())
+            data.block_action = [{ action: 'crack_break', position: { x, y, z }, face }]
+            data.input_data.block_action = true
+          })
         } else {
           clearInterval(cracks)
-          const actions3 = [{ action: 'stop_break' }, { action: 'crack_break', position: { x, y, z }, face: 2 }]
+          client.once('pre_player_auth_input', (eventTwo) => {
+            const data = eventTwo.data
 
-          const packet3 = new ClientAuthInput(client, actions3, position, true)
-          client.queue('player_auth_input', packet3.create())
+            data.block_action = [{ action: 'stop_break' }, { action: 'crack_break', position: { x, y, z }, face }]
+            data.input_data.block_action = true
+            data.input_data.item_interact = true
+            data.transaction = {
+              legacy: {
+                legacy_request_id: 0
+              },
+              actions: [],
+              data: {
+                action_type: 'break_block',
+                block_position: { x, y, z },
+                face,
+                hotbar_slot: 0,
+                held_item: { network_id: 0 },
+                player_pos: client.data.position,
+                click_pos: { x: 0, y: 0, z: 0 },
+                block_runtime_id: 0
+              }
+            }
+          })
+
           await sleep(50)
           resolve()
         }
@@ -47,21 +70,37 @@ module.exports = (client) => {
 }
 
 /**
- * Calculate PitchAndYaw. This is not accurate i think but helps
- *
- * @param {{x: number, y: number, z: number}} playerPosition
- * @param {{x: number, y: number, z: number}} blockPosition
- * @returns {{pitch: number, yaw: number}}
+ * Calculates pitch and yaw.
+ * @param {Object} playerPosition - The player's position {x, y, z}.
+ * @param {Object} blockPosition - The block's position {x, y, z}.
+ * @returns {Object}
  */
 function calculatePitchAndYaw (playerPosition, blockPosition) {
-  const dx = blockPosition.x - playerPosition.x
-  const dy = blockPosition.y - playerPosition.y
-  const dz = blockPosition.z - playerPosition.z
+  const deltaX = blockPosition.x - playerPosition.x
+  const deltaY = blockPosition.y - (playerPosition.y + 1.65)
+  const deltaZ = blockPosition.z - playerPosition.z
 
-  const yaw = Math.atan2(dz, dx) * (180 / Math.PI)
+  const angle = Math.atan2(deltaZ, deltaX)
+  const degrees = angle * (180 / Math.PI)
+  const direction = Math.round(((degrees + 180) % 360) / 90) % 4
 
-  const horizontalDistance = Math.sqrt(dx * dx + dz * dz)
-  const pitch = -Math.atan2(dy, horizontalDistance) * (180 / Math.PI)
+  let yaw
+  switch (direction) {
+    case 0:
+      yaw = 180 // South
+      break
+    case 1:
+      yaw = 270 // West
+      break
+    case 2:
+      yaw = 0 // North
+      break
+    case 3:
+      yaw = 90 // East
+      break
+  }
 
+  const horizontalDistance = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ)
+  const pitch = Math.atan2(deltaY, horizontalDistance) * (180 / Math.PI)
   return { pitch, yaw }
 }
