52dec0120620589c838ceea3475b8255e431b0d7
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-02-23 02:10:02 +0200

    Level chunk event

---
+++ src/events/LevelChunk.js
@@ -0,0 +1,13 @@
+/** @type {import("../../types/events/Event").Event} */
+module.exports = {
+  name: 'level_chunk',
+  once: false,
+  execute: async (params, client) => {
+    const ChunkColumn = require('prismarine-chunk')(client.registry)
+    const column = new ChunkColumn({ x: params.x, z: params.z })
+    const payload = Buffer.from(params.payload)
+
+    await column.networkDecodeNoCache(payload, params.sub_chunk_count)
+    client.data.world.columns[`${params.x}:${params.z}`] = column
+  }
+}
