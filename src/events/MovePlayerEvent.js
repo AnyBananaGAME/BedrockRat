93eeafc3e4fc0512dcfddb8e0cabb1406012162b
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-02-23 02:10:29 +0200

    MovePlayer Event updates

--- src/events/MovePlayerEvent.js
+++ src/events/MovePlayerEvent.js
@@ -1,12 +1,20 @@
+const { fromNotchianYaw, fromNotchianPitch } = require('../utils/conversions')
+
 module.exports = {
   name: 'move_player',
   once: false,
   execute: async (params, client) => {
-    if (BigInt(params.runtime_id) !== client.data.runtime_entity_id) return
-    if (params.mode !== 'teleport') return
+    if (params.runtime_id !== parseInt(client.data.runtime_entity_id) && params.mode === 'teleport') {
+      return
+    }
     const position = client.data.position
     position.x = Number(params.position.x)
     position.y = Number(params.position.y)
     position.z = Number(params.position.z)
+
+    client.playerState.teleportTicks = 5
+    client.playerState.pos.set(params.position.x, params.position.y - 1.621, params.position.z)
+    client.playerState.yaw = fromNotchianYaw(params.yaw)
+    client.playerState.pitch = fromNotchianPitch(params.pitch)
   }
 }
